ed1ac2da727bb0aeac661c641b58e43400849313
Freakadel <t_c@gmx.net>
2020-02-21 23:31:47 +0100

    afuberechnung: Erstellung der Oberflaeche

--- afuberechnung.cpp
+++ afuberechnung.cpp
@@ -3,6 +3,7 @@
 AfuBerechnung::AfuBerechnung(QWidget *parent)
     : QMainWindow(parent)
 {
+
 }
 
 AfuBerechnung::~AfuBerechnung()
@@ -12,6 +13,68 @@ AfuBerechnung::~AfuBerechnung()
 
 void AfuBerechnung::Programm()
 {
-//    QWidget *central  = new QWidget(this);
-//    QMenuBar dateiMenu =
+    QMenuBar *menuDatei = new QMenuBar;
+    menuDatei->addAction(actionNeu);
+    menuDatei->addAction(actionOeffnen);
+    menuDatei->addAction(actionSpeichern);
+    menuDatei->addSeparator();
+    menuDatei->addAction(actionBeenden);
+
+    menuOptionen = new QMenu(this);
+    menuOptionen->addAction(actionFrequenz);
+    menuOptionen->addAction(actionKapazitaet);
+    menuOptionen->addAction(actionInduktivitaet);
+
+    menuHilfe = new QMenu(this);
+    menuHilfe->addAction(actionInfo);
+
+
+    connect(actionNeu, SIGNAL(triggered()), this, SLOT(triggeredNeu()));
+    connect(actionNeu, SIGNAL(triggered()), this, SLOT(triggeredOeffnen()));
+    connect(actionNeu, SIGNAL(triggered()), this, SLOT(triggeredSpeichern()));
+    connect(actionNeu, SIGNAL(triggered()), this, SLOT(triggeredBeenden()));
+
+}
+
+void AfuBerechnung::triggeredNeu()
+{
+
+}
+
+void AfuBerechnung::triggeredOeffnen()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Öffne Datei.");
+    QFile file(fileName);
+    QString currentFile = fileName;
+
+    if (!file.open(QIODevice::ReadOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warnung!", "Datei konnte nicht geöffnet werden." + file.errorString());
+        return;
+    }
+
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    file.close();
+}
+
+void AfuBerechnung::triggeredSpeichern()
+{
+
+}
+
+void AfuBerechnung::triggeredBeenden()
+{
+    close();
+}
+
+void AfuBerechnung::triggeredInfo()
+{
+    QMessageBox::about(this, tr("Über Formelberechnung von Thomson"),
+                       tr("<h2>Gleichungsberechnung 1.0</h2>"
+                          "<p>Copyright &copy; 2020 by DF1TC."
+                          "<p>Für Amateurfunkanwendungen ist dieses Programm"
+                          " Freeware, eine kommerzielle Nutzung ist ausschließlich mit der"
+                          " Zustimmung des Autors gestattet!"));
 }
--- afuberechnung.h
+++ afuberechnung.h
@@ -14,6 +14,10 @@
 #include <QAction>
 #include <QMessageBox>
 #include <QIcon>
+#include <QString>
+#include <QFile>
+#include <QTextStream>
+#include <QFileDialog>
 
 class QAction;
 class QLabel;
@@ -22,24 +26,53 @@ class AfuBerechnung : public QMainWindow
 {
     Q_OBJECT
 
+private:
+    QMenuBar *menuDatei;
+    QMenu *menuBearbeiten;
+    QMenu *menuHilfe;
+    QMenu *menuOptionen;
+
+    QAction *actionNeu = new QAction(tr("Neu"), this);
+    QAction *actionOeffnen = new QAction(tr("Öffnen"), this);
+    QAction *actionSpeichern = new QAction(tr("Speichern"), this);
+    QAction *actionBeenden = new QAction(tr("Beenden"), this);
+    QAction *actionFrequenz = new QAction(tr("Frequenz berechnen"), this);
+    QAction *actionKapazitaet = new QAction(tr("Kapazität berechnen"), this);
+    QAction *actionInduktivitaet = new QAction(tr("Induktivität berechnen"), this);
+    QAction *actionInfo = new QAction(tr("Info"), this);
+
+    QLineEdit *LineEdit_f;
+    QLineEdit *LineEdit_c;
+    QLineEdit *LineEdit_l;
+    QLabel *Label_f_Loesung;
+
 public:
     AfuBerechnung(QWidget *parent = 0);
     ~AfuBerechnung();
 
+    bool b_FChoice = false;
+    bool b_CChoice = false;
+    bool b_LChoice = false;
+
+    double const d_Pi = 3.141592653589793238462643383279;
+    double d_Frequenz {1.0};
+    double d_Kapazitaet {1.0};
+    double d_Induktivitaet {1.0};
+    double d_CEingabe {0.0};
+    double d_FEingabe {0.0};
+    double d_LEingabe {0.0};
+
     void Programm();
 
 protected:
-    void closeEvent(QCloseEvent *event);
+    //void closeEvent(QCloseEvent *event);
 
 private slots:
-    void dateiOeffnen();
-    void dateiSpeichern();
-    void dateiBeenden();
-
-    void hilfeInfo();
-
-
-
+    void triggeredNeu();
+    void triggeredOeffnen();
+    void triggeredSpeichern();
+    void triggeredBeenden();
+    void triggeredInfo();
 };
 
 #endif // AFUBERECHNUNG_H
